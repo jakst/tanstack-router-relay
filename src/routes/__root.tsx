5bdd6734ded762b8a879f1a23e3b6a65009bf053
Jakob Norlin <jakob.stahl91@gmail.com>
2024-03-13 12:08:33 +0100

    Format files

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,36 +1,36 @@
-import * as React from 'react'
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router"
+import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import * as React from "react"
 
 export const Route = createRootRoute({
-  component: RootComponent,
+	component: RootComponent,
 })
 
 function RootComponent() {
-  return (
-    <>
-      <div className="p-2 flex gap-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>{' '}
-        <Link
-          to={'/about'}
-          activeProps={{
-            className: 'font-bold',
-          }}
-        >
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
-  )
+	return (
+		<>
+			<div className="p-2 flex gap-2 text-lg">
+				<Link
+					to="/"
+					activeProps={{
+						className: "font-bold",
+					}}
+					activeOptions={{ exact: true }}
+				>
+					Home
+				</Link>{" "}
+				<Link
+					to={"/about"}
+					activeProps={{
+						className: "font-bold",
+					}}
+				>
+					About
+				</Link>
+			</div>
+			<hr />
+			<Outlet />
+			<TanStackRouterDevtools position="bottom-right" />
+		</>
+	)
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router"
+import * as React from "react"
 
-export const Route = createFileRoute('/')({
-  component: HomeComponent,
+export const Route = createFileRoute("/")({
+	component: HomeComponent,
 })
 
 function HomeComponent() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+	return (
+		<div className="p-2">
+			<h3>Welcome Home!</h3>
+		</div>
+	)
 }
